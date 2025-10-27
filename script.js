3f9bf15129cac7311155ca36b77b7fe46ff1bd75
bmouwcode <144856537+bmouwcode@users.noreply.github.com>
2025-10-27 19:35:52 -0400

    add some fun functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("mainForm");
+const log = document.getElementById("log");
+
+form.addEventListener("submit", (event) => {
+  const data = new FormData(form);
+  let output = "";
+  for (const entry of data) {
+    output = `${output}${entry[0]}=${entry[1]}\r`;
+  }
+  log.innerText = output;
+  event.preventDefault();
+});
