16c709e557924f538e3ca542e3145dacf35e054d
bmouwcode <144856537+bmouwcode@users.noreply.github.com>
2025-11-10 17:42:53 -0500

    maybe fix...

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const form = document.getElementById("mainForm");
 const log = document.getElementById("log");
 
-form.addEventListener("submit", (event) => {
-  const data = new FormData(form);
-  let output = "";
-  for (const entry of data) {
-    output = `${output}${entry[0]}=${entry[1]}\r`;
-  }
-  log.innerText = data.get("yes1");
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const log = document.getElementById("log");
+  form.addEventListener("submit", (event) => {
+    const data = new FormData(form);
+    let output = "";
+    for (const entry of data) {
+      output += `${entry[0]}=${entry[1]}\r`;
+    }
+    log.innerText = data.get("yes1");
+    event.preventDefault();
+  });
 });
+
