1b47b3f14000023689e9d9f574cfbecb2fe634a3
bmouwcode <144856537+bmouwcode@users.noreply.github.com>
2025-11-10 19:27:22 -0500

    IT WORKS! (at least the previous one)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("mainForm");
   const log = document.getElementById("log");
+  const style = document.querySelector("style");
   form.addEventListener("submit", (event) => {
     const data = new FormData(form);
     let output = "";
@@ -9,14 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
       output += `${entry[0]}=${entry[1]}\r`;
     }
     
-    
+
     const selectedOption = data.get("options");
     if (selectedOption) {
       log.innerText = `HI ${selectedOption} HI2`;
     } else {
       log.innerText = "HI No option selected HI2";  // Fallback message if no option is selected
     }
-
+    if(selectedOption === "banana") {
+      style.textContent += "body {background-color:yellow;}";
+    }
+    else{
+      style.textContent += "body {background-color:purple;}";
+    }
 
     event.preventDefault();
   });
