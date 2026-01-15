407ceeb3734b593cf21dd6d740815eb2064e45f4
bmouwcode <144856537+bmouwcode@users.noreply.github.com>
2026-01-15 18:58:55 -0500

    Button functionality (hopefully)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let pages = ["setup", "auto", "teleop", "endgame", "export"];
 let currPage = "setup";
+function cycleRight() {
+    if(pages.indexOf(currPage) < pages.length - 1) {
+        document.getElementById(currPage).hidden = true;
+        currPage = pages[pages.indexOf(currPage) + 1];
+        document.getElementById(currPage).hidden = false;
+    }
+}
+function cycleLeft() {
+    if(pages.indexOf(currPage) > 0) {
+        document.getElementById(currPage).hidden = true;
+        currPage = pages[pages.indexOf(currPage) - 1];
+        document.getElementById(currPage).hidden = false;
+    }
+}
+document.getElementById("floatright").onclick = cycleRight;
+document.getElementById("floatleft").onclick = cycleLeft;
