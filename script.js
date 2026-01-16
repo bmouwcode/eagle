15d542cd8367a8f736cdea18174142c592eea6a3
bmouwcode <144856537+bmouwcode@users.noreply.github.com>
2026-01-15 19:00:52 -0500

    It works! + comments in js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//List of pages on the main site
 let pages = ["setup", "auto", "teleop", "endgame", "export"];
+//the page the site is currently on
 let currPage = "setup";
+//function to move one page right
 function cycleRight() {
     if(pages.indexOf(currPage) < pages.length - 1) {
         document.getElementById(currPage).hidden = true;
@@ -7,6 +10,7 @@ function cycleRight() {
         document.getElementById(currPage).hidden = false;
     }
 }
+//funtion to move one page left
 function cycleLeft() {
     if(pages.indexOf(currPage) > 0) {
         document.getElementById(currPage).hidden = true;
@@ -14,5 +18,6 @@ function cycleLeft() {
         document.getElementById(currPage).hidden = false;
     }
 }
+//linking the functions to the buttons
 document.getElementById("floatright").onclick = cycleRight;
 document.getElementById("floatleft").onclick = cycleLeft;
